c2a7aa76bd3d469f8fd051d198e8b9d2d112198c
gowtham-32112 <140431497+gowtham-32112@users.noreply.github.com>
2024-04-27 07:20:35 +0530

    server codes

--- index.js
+++ index.js
@@ -1,20 +1,83 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
- <BrowserRouter>
- 
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </BrowserRouter>
-);
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ObjectId } = require('mongodb');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const app = express();
+
+app.use(cors()); // Enable CORS
+app.use(express.json());
+
+const client = new MongoClient('mongodb+srv://admin1:admin1@cluster0.15mlbnn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+
+async function startServer() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+
+    const db = client.db('counselling');
+    const col = db.collection('register');
+
+    app.post('/register', async (req, res) => {
+      try {
+        await col.insertOne(req.body); // Wait for insertion to complete
+        res.send('Data inserted successfully');
+      } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+
+    app.get('/retrieve', async (req, res) => {
+      try {
+        const result = await col.find().toArray(); // Wait for data retrieval to complete
+        console.log(result);
+        res.send(result);
+      } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+
+    app.put('/users/:id', async (req, res) => {
+      try {
+        const { id } = req.params;
+        const { name, role, email, password } = req.body;
+        const result = await col.updateOne(
+          { _id: new ObjectId(id) },
+          { $set: { name, role, email, password } }
+        );
+        res.send('Data updated successfully');
+      } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+
+    app.delete('/users/:id', async (req, res) => {
+      try {
+        const { id } = req.params;
+        const result = await col.deleteOne({ _id: new ObjectId(id) });
+        res.json({ message: "Data deleted successfully" });
+      } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+
+    app.get('/', (req, res) => {
+      res.send('Hello World');
+    });
+
+    app.get('/about', (req, res) => {
+      res.send('<h1>About Us</h1>');
+    });
+
+    app.listen('8080', () => {
+      console.log('Server is running on port 8080');
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+startServer();
